efcafc6128f3af03efb1ca7fa6c185be677089c4
JavierBarreto <javierbarreto1026@gmail.com>
2025-10-06 18:23:17 -0500

    Update habitaciones.py
    creacion de la clase habitacion

--- habitaciones.py
+++ habitaciones.py
@@ -1,1 +1,9 @@
-print("Hello World")
+#clase de la habitacion
+class Habitaciones:
+  def __init__(self, numero, tipo):
+    self.numero = numero
+    self.tipo = tipo
+    self.estado = "DISPONIBLE"
+
+  def __str__(self):
+    return f"Habitacion {self.numero} ({self.tipo}) - {self.estado}"
