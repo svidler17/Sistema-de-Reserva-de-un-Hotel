3f05317493831d4e0838628899e656b0d2527359
JavierBarreto <javierbarreto1026@gmail.com>
2025-10-15 15:50:11 -0500

    Final habitaciones.py

--- habitaciones.py
+++ habitaciones.py
@@ -1,29 +1,13 @@
-# =========================================================
-#  Clase Habitación
-# =========================================================
 class Habitacion:
     def __init__(self, numero, tipo, tarifa):
         self.numero = numero
         self.tipo = tipo
         self.tarifa = tarifa
         self.estado = "Disponible"
-        self.dias_estadia = 0
-        self.metodo_pago = None
-        self.total = 0
-
-    def calcular_total(self):
-        if self.dias_estadia <= 0:
-            self.dias_estadia = 1
-        self.total = self.dias_estadia * self.tarifa
-        return self.total
 
     def cambiar_estado(self, nuevo_estado):
-        if nuevo_estado not in ["Disponible", "Ocupada", "Mantenimiento"]:
-            print("Estado no válido.")
-        else:
+        if nuevo_estado in ["Disponible", "Ocupada"]:
             self.estado = nuevo_estado
-            print(f"Estado actualizado a: {self.estado}")
 
     def __str__(self):
-        return (f"Habitación {self.numero} ({self.tipo}) - {self.estado} | "
-                f"Días: {self.dias_estadia} | Total: ${self.total}")
+        return f"Habitación {self.numero} ({self.tipo}) - {self.estado} | Tarifa: ${self.tarifa}"
