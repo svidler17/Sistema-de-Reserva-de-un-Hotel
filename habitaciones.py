8d3058a43c57441eef03689ec5a9d1f2b026bb8b
JavierBarreto <javierbarreto1026@gmail.com>
2025-10-16 21:09:56 -0500

    Update Final habitaciones.py

--- habitaciones.py
+++ habitaciones.py
@@ -1,13 +1,16 @@
 class Habitacion:
-    def __init__(self, numero, tipo, tarifa):
+    def _init_(self, numero, tipo, tarifa):
         self.numero = numero
         self.tipo = tipo
         self.tarifa = tarifa
         self.estado = "Disponible"
 
     def cambiar_estado(self, nuevo_estado):
-        if nuevo_estado in ["Disponible", "Ocupada"]:
+        estados_validos = ["Disponible", "Ocupada", "Mantenimiento"]
+        if nuevo_estado in estados_validos:
             self.estado = nuevo_estado
+        else:
+            print("Estado no válido. Intente de nuevo.")
 
-    def __str__(self):
+    def _str_(self):
         return f"Habitación {self.numero} ({self.tipo}) - {self.estado} | Tarifa: ${self.tarifa}"
